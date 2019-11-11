44b6b01b01f35d100df885a246c56e578f85a020
Sonya <55147091+pobiedimska@users.noreply.github.com>
2019-11-12 01:24:42 +0200

    Добавлены валидаторы

--- laboratory1/task3.py
+++ laboratory1/task3.py
@@ -1,6 +1,20 @@
-x = float(input('Введіть x: '))
-f = 0
+"""
+Обчислити функцію в точці
+"""
+import re
 
+float_pattern = re.compile("^\d*(\.\d+)?$")
+
+#validator
+def validate_arg(pattern):
+	arg = input("Введіть x: ")
+	while not bool(pattern.match(arg)):
+		arg = input("Для значень неможливо обчислити функцію, спробуйте ще раз.\nВведіть х: ")
+	arg = float(arg)
+	return arg
+
+x = validate_arg(float_pattern)
+f = 0
 if x > 3:
     f = 1.2*(x**2)-3*x-9
 else:

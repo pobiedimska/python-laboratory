2d6f38db555f78c05d1ef621290e469c794d015c
Sonya <55147091+pobiedimska@users.noreply.github.com>
2019-11-12 00:03:36 +0200

    Добавлен валидатор

--- laboratory1/task1.py
+++ laboratory1/task1.py
@@ -3,9 +3,24 @@
 Потрібно виконати над ними побітові операції І, АБО і виключення АБО. 
 В кінці вивести результат операцій також в двійковому поданні. 
 """
+import re
 
-a = int(input("Введіть перше число: "), 2)
-b = int(input("Введіть друге число: "), 2)
+integer_pattern = re.compile("^[-+]{0,1}[0,1]+$")
+
+#validator
+def input_number(pattern, n):
+	number = input("Введіть " + str(n) + " число у двійковій системі числення: ")
+	while not bool(pattern.match(nuber)):
+        number = input("З таким значенням неможливо виконати задачу. Спробуйте ще раз.\nВведіть " + str(n) + " число: ")
+	return number
+	
+#convertion to decimal number system
+def convert_to_dec(n):
+    number = int(input_number(integer_pattern, n), 2)
+    return number
+	
+a = convert_to_dec(1)
+b = convert_to_dec(2)
 
 print("Результат побітовою операці І:", str(bin(a&b)))
 print("Результат побітовою операці АБО:", str(bin(a|b)))
